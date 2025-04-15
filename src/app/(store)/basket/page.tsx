18fffa31f62edfa260077b90bdb2d7b1528de4e2
mridulmahajan04 <mridulmahajan16@gmail.com>
2025-04-15 12:26:37 +0530

    Add To Cart

--- src/app/(store)/basket/page.tsx
+++ src/app/(store)/basket/page.tsx
@@ -5,6 +5,9 @@ import React from 'react'
 import { useAuth, useUser } from "@clerk/nextjs";
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import AddToBasketButton from '@/components/AddToBasketButton';
+import { imageUrl } from '@/lib/imageUrl';
+import Image from 'next/image';
 const page = () => {
     const groupedItems = useBasketStore((state) => state.getGroupItems());
     const { isSignedIn } = useAuth();
@@ -13,7 +16,6 @@ const page = () => {
 
     const [isClient, setIsClient] = useState(false);
     const [loading, setLoading] = useState(false);
-    if (!isClient) return null;
     if (groupedItems.length == 0) {
         return (
             <div className='container mx-auto p-4 flex flex-col items-center justify-center min-h-[50vh]'>
@@ -28,10 +30,32 @@ const page = () => {
             <div className='flex flex-col lg:flex-row gap-8'>
                 <div className='flex-grow'>
                     {groupedItems?.map((item) => (
-                    <div
-                        key={item.product._id} className='mb-4 p-4 border rounded flex items-center justify-between'>
-                            <div>{item.product.name}</div>
-                            <div>{item.quantity}</div>
+                        <div
+                            key={item.product._id} className='mb-4 p-4 border rounded flex items-center justify-between'>
+                            <div className='flex items-center cursor-pointer flex-1 min-w-0' onClick={() => {
+                                router.push(`/product/${item.product.slug?.current}`)
+                            }}>
+
+                                <div className='w-20 h-20 sm:w-24 sm:h-24 flex-shirk-0 mr-4'>
+                                    {item.product.image && (
+                                        <Image src={imageUrl(item.product.image).url()} alt={item.product.name ?? "Product Image"} className="w-full h-full object-cover rounded" width={96}
+                                            height={96} />
+
+                                    )}
+                                </div>
+
+                                <div className='min-w-0'>
+                                    <h2 className='text-lg sm:text-xl font-semibold truncate'>{item.product.name}</h2>
+                                    <p className='text-sm sm:text-base'>
+                                        Price: Rs. 
+                                        {((item.product.price ?? 0) * item.quantity).toFixed(2)}
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className='flex items-center ml-4 flex-shrink-0'>
+                                <AddToBasketButton product={item.product} disabled={false}></AddToBasketButton>
+                            </div>
                         </div>
                     ))}
                 </div>
